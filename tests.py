92331fd5ff2c00a65495d655bca66dedc42854d3
Cezary <102999499+panpusto@users.noreply.github.com>
2022-09-22 21:09:36 +0200

    increment string with test with pytest

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import pytest
 
+from exercises.increment_string import increment_string
 from exercises.summation import summation
 from exercises.intro_bites_08_by_PyBites import BeltStats, ninja_belts, get_total_points
 from exercises.intro_bite_09_by_PyBites import get_workout_motd, INVALID_DAY, CHILL_OUT
@@ -47,3 +48,13 @@ def test_get_total_points_more_belts():
 def test_get_workout_valid_case_insensitive_dict_lookups(day, expected):
     assert get_workout_motd(day) == expected
 
+
+#   test for increment_string.py
+def test_increment_string():
+    assert increment_string("foo") == "foo1"
+    assert increment_string("foobar001") == "foobar002"
+    assert increment_string("foobar1") == "foobar2"
+    assert increment_string("foobar00") == "foobar01"
+    assert increment_string("foobar99") == "foobar100"
+    assert increment_string("foobar099") == "foobar100"
+    assert increment_string("") == "1"
